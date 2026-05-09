b7ecf3f15ad4869c2405245d55d9e21942847683
Arshiya <arshiyauppal05@gmail.com>
2026-05-09 13:25:11 +0530

    Added exception handling to the project

--- NATO_alphabets/main.py
+++ NATO_alphabets/main.py
@@ -1,9 +1,28 @@
 import pandas
 
-data = pandas.read_csv("/Users/arshiya/Desktop/Coding/Python/projects-python/NATO_alphabets/nato_phonetic_alphabet.csv")
+data = pandas.read_csv("/Users/arshiya/Desktop/Coding/Python/Python_Projects/NATO_alphabets/nato_phonetic_alphabet.csv")
 nato_dict = {row["letter"]:row.code for (index, row) in data.iterrows()}
 #you can use both notations, row.column_name or row["column_name"]
 
-user_input = input("Enter your name: ").upper()
-code_words = [nato_dict[string] for string in user_input]
-print(code_words)
+# is_on = True
+# while is_on:
+#     user_input = input("Enter your name: ").upper()
+#     try:
+#         code_words = [nato_dict[string] for string in user_input]
+#         is_on = False
+#     except KeyError:
+#         print("Sorry, letters in the alphabet please.\n")
+#     else:
+#         print(code_words)
+
+def generate_phonetic():
+    user_input = input("Enter your name: ").upper()
+    try:
+        code_words = [nato_dict[string] for string in user_input]
+    except:
+        print("Sorry, letters in the alphabet please.\n")
+        generate_phonetic()
+    else:
+        print(code_words)
+
+generate_phonetic()
